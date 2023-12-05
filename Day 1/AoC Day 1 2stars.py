963d9b69a9453b595033e8ed3764c8cc45313df2
Mleczna <76711768+Mleczna@users.noreply.github.com>
2023-12-06 00:08:26 +0100

    AoC Day 1 2stars.py

--- Day 1/AoC Day 1 2stars.py
+++ Day 1/AoC Day 1 2stars.py
@@ -6,6 +6,27 @@ linia = ""
 
 for x in passwords:
 
+    linia = x.replace("one", "o1e")
+    x = linia
+    linia = x.replace("two", "t2o")
+    x = linia    
+    linia = x.replace("three", "t3hee")
+    x = linia 
+    linia = x.replace("four", "f4ur")
+    x = linia 
+    linia = x.replace("five", "f5ve")
+    x = linia 
+    linia = x.replace("six", "s6x")
+    x = linia 
+    linia = x.replace("seven", "s7ven")
+    x = linia 
+    linia = x.replace("eight", "e8ght")
+    x = linia 
+    linia = x.replace("nine", "n9ne")
+    x = linia 
+
+    print(x)
+
     for i in x:
         if i.isdigit():
             tab.append(i)
